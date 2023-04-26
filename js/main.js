ac2f1894fd4f8873d83782e5f1802ce08f0447ee
kimsanng <kimgod3210@naver.com>
2023-04-27 04:44:41 +0900

    마지막이길

--- js/main.js
+++ js/main.js
@@ -31,7 +31,38 @@ $(function(){
 });
 
 
+$(function(){
 
+    $('.portpolio2_slider').slick({
+          arrows: true,
+          dots: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          responsive: [
+            {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: true,
+            arrows: false
+          }
+        },
+        {
+                  breakpoint: 768,
+                  settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1,
+                      arrows: false,
+                      dots: true
+                  }
+              }
+          ]
+      })
+  })
 
 
 
@@ -85,37 +116,7 @@ toTopEl.addEventListener('click', function(){
     });
 })
 
-$(function(){
 
-    $('.portpolio2_slider').slick({
-          arrows: true,
-          dots: true,
-          infinite: true,
-          speed: 300,
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          responsive: [{
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: false,
-            dots: true,
-            arrows: false
-          }
-        },
-        {
-                  breakpoint: 768,
-                  settings: {
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                      arrows: false,
-                      dots: true
-                  }
-              }
-          ]
-      });
-  })
 
 
 
