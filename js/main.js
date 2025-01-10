2821a6660a7eb7301b1e191e71ef0682ac853c45
kimsangkeun <kimgod321@naver.com>
2025-01-10 17:28:05 +0900

    port

--- js/main.js
+++ js/main.js
@@ -70,25 +70,25 @@ $(function(){
 
 
 
-const loader = document.querySelector('.loader');
-const html = document.querySelector('html');
+// const loader = document.querySelector('.loader');
+// const html = document.querySelector('html');
 
-html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
+// html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
 
-window.addEventListener('load', ()=>{
+// window.addEventListener('load', ()=>{
 
-     setTimeout(() => { //  <-* 로딩속도를 구현하기 위한 코드로 실제 적용시 제거
+//      setTimeout(() => { //  <-* 로딩속도를 구현하기 위한 코드로 실제 적용시 제거
     
-      	loader.style.opacity = '0';
-		html.style.overflow = 'auto'; //스크롤 방지 해제
+//       	loader.style.opacity = '0';
+// 		html.style.overflow = 'auto'; //스크롤 방지 해제
       	
-        setTimeout(() => {
-      		loader.style.display = 'none';
-  		}, 300);
+//         setTimeout(() => {
+//       		loader.style.display = 'none';
+//   		}, 300);
         
-  }, 2000); // <-* 로딩속도 구현
+//   }, 2000); // <-* 로딩속도 구현
 
-})
+// })
 
 // lodash
 
